7b7e80b8b88a3134af113bbed9517dd3e9999ccd
MuhammadAnbiya <anbiya17agustus@gmail.com>
2026-02-06 03:44:52 +0700

    Day 4 Learn C++ - Commit 1

---
+++ day-4/cc6_strigs.cpp
@@ -0,0 +1,15 @@
+/*
+Declare a string variable named message
+Store the text Hello in the string
+Print the string using cout
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string message = "Hello";
+	cout << message << endl;
+	return 0;
+}
---
+++ day-4/string_cStyle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string greeting1 = "Hello";
+	char greeting2[] = "Hello";
+
+	cout << greeting1 << endl;
+	cout << greeting2 << endl;
+	return 0;
+}
